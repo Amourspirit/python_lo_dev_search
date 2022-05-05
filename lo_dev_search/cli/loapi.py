7d0d7e60e2e196e1150d7f5791c849c72ce5c3c7
Paul Moss <vibrationoflife@protonmail.com>
2022-05-05 13:34:25 -0400

    loapi comp subparser

--- lo_dev_search/cli/loapi.py
+++ lo_dev_search/cli/loapi.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 # coding: utf-8
 import webbrowser
 import sys
@@ -7,15 +6,18 @@ from typing import List, Union
 from ..api_search import search_api
 from ..data_class.component import Component
 
+# region Terminal Questions
+
+
 def query_choice(comps: List[Component]) -> Union[int, None]:
-    """Ask a yes/no question via raw_input() and return their answer.
+    """
+    Ask for a choice of wich component to open url for.
 
-    "question" is a string that is presented to the user.
-    "default" is the presumed answer if the user just hits <Enter>.
-            It must be "yes" (the default), "no" or None (meaning
-            an answer is required of the user).
+    Arguments:
+        comps (List[Component]): List of components to choose from.
 
-    The "answer" return value is True for "yes" or False for "no".
+    Returns:
+        (Union[int, None]): Integer representing the zero base index within comps or None if canceled.
     """
     # https://tinyurl.com/yyg38fp2
     # https://tinyurl.com/y2pv2cdh
@@ -23,22 +25,19 @@ def query_choice(comps: List[Component]) -> Union[int, None]:
     # valid is 1 to length of comps + 1 for None
     valid = tuple([i for i in range(1, c_len + 1)])
     question = 'Choose an option (default 1):'
-    prompt = '\n[0], Cancel'
-    i = 1
-    for comp in comps:
-        # if i == 1:
-        #     prompt = prompt + f"\n[{i}] (default)), {comp.name}, {comp.id_component}"
-        # else:
-        prompt = prompt + f"\n[{i}], {comp.name}, {comp.id_component}"
+    prompt = f'\n{"[0],":<5} Cancel'
+    for i, comp in enumerate(comps, 1):
+        prompt = prompt + \
+            f"\n{f'[{i}],':<5} {comp.name:<33} - {comp.id_component}"
         i += 1
-    # prompt += f"\n[{i}] Cancel\n"
     prompt += '\n'
-    # last = i
     while True:
         sys.stdout.write(question + prompt)
         choice = input().lower()
         if choice == '':
             return 0
+        if choice == 'q':
+            return None
         if choice.isdigit():
             j = int(choice)
         else:
@@ -48,16 +47,80 @@ def query_choice(comps: List[Component]) -> Union[int, None]:
         if j in valid:
             return j - 1
         else:
-            choices = ','.join([str(i) for i in range(c_len + 1)])
-            sys.stdout.write(f"Please respond with one of the following: {choices}\n")
+            sys.stdout.write(f"Please respond with input from 0 - {c_len}\n")
+# endregion Terminal Questions
 
-def main() -> int:
-    results = search_api.search_component("%time%", limit=15)
+
+def _args_comp(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        '-s', '--search',
+        help=f"",
+        action='store',
+        dest='search',
+        required=True
+    )
+    parser.add_argument(
+        '-l', '--no-leading',
+        help="No leading wildcard in search",
+        action='store_false',
+        dest='leading',
+        default=True
+    )
+    parser.add_argument(
+        '-t', '--no-trailing',
+        help="No trailing wildcard in search",
+        action='store_false',
+        dest='trailing',
+        default=True
+    )
+    parser.add_argument(
+        '-m', '--max-results',
+        help="Limits the number of results returned: Default 10",
+        action='store',
+        dest='limit',
+        type=int,
+        default=10
+    )
+
+
+def _args_comp_action(args: argparse.Namespace) -> int:
+    search = "%".join(str(args.search).split())
+    # print(search)
+    if len(search) <= 2:
+        print("Search requires at least 3 characters")
+        return 0
+    if args.leading:
+        search = "%" + search
+    if args.trailing:
+        search = search + '%'
+    results = search_api.search_component(search, limit=int(args.limit))
+    if len(results) == 0:
+        print('Search produced no results')
+        return 0
     choice = query_choice(results)
     if choice is not None:
         url = results[choice].url
         webbrowser.open(url)
-    # for result in results:
-    #     print(f"{result.name}, {result.id_component}")
-    
+    return 0
+
+
+def _args_process_cmd(args: argparse.Namespace) -> int:
+    if args.command == "comp":
+        return _args_comp_action(args)
+    return 0
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser(description='main')
+    subparser = parser.add_subparsers(dest="command")
+    cmd_comp = subparser.add_parser(
+        name="comp", help="Search for components")
+    _args_comp(cmd_comp)
+    if len(sys.argv) <= 1:
+        parser.print_help()
+        return 0
+    args = parser.parse_args()
+    print(args)
+    _args_process_cmd(args)
+
     return 0
--- lo_dev_search/cli/loguide.py
+++ lo_dev_search/cli/loguide.py
@@ -3,26 +3,43 @@
 import argparse
 from ..web_search.search_guide import search, SearchDevEnum
 
+
 def _get_help(app_name: str) -> str:
     return f"Search {app_name} developer guide for supplied search terms."
 
 # region parser
 # region    Add Arguments
+
+
 def _args_cmd_writer(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('search', nargs='*', help=_get_help("Writer"))
+
+
 def _args_cmd_calc(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('search', nargs='*', help=_get_help("Calc"))
+
+
 def _args_cmd_draw(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('search', nargs='*', help=_get_help("Draw"))
+
+
 def _args_cmd_chart(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('search', nargs='*', help=_get_help("Chart"))
+
+
 def _args_cmd_base(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('search', nargs='*', help=_get_help("Base"))
+
+
 def _args_cmd_form(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('search', nargs='*', help=_get_help("Form"))
+
+
 def _args_cmd_general(parser: argparse.ArgumentParser) -> None:
-    parser.add_argument('search', nargs='*', help="Search developer guide for supplied search terms.")
+    parser.add_argument(
+        'search', nargs='*', help="Search developer guide for supplied search terms.")
 # endregion    Add Arguments
+
 
 def _args_process_cmd(
     args: argparse.Namespace
@@ -45,16 +62,24 @@ def _args_process_cmd(
         search(SearchDevEnum.DEV, get_search())
 # endregion parser
 
+
 def main() -> int:
     parser = argparse.ArgumentParser(description='main')
     subparser = parser.add_subparsers(dest="command")
-    cmd_writer = subparser.add_parser(name="writer", help="Search Writer developer guide")
-    cmd_calc = subparser.add_parser(name="calc", help="Search Calc developer guide")
-    cmd_draw = subparser.add_parser(name="draw", help="Search Draw developer guide")
-    cmd_chart = subparser.add_parser(name="chart", help="Search Chart developer guide")
-    cmd_base = subparser.add_parser(name="base", help="Search Base developer guide")
-    cmd_form = subparser.add_parser(name="form", help="Search Form developer guide")
-    cmd_general = subparser.add_parser(name="general", help="Search all of developer guide")
+    cmd_writer = subparser.add_parser(
+        name="writer", help="Search Writer developer guide")
+    cmd_calc = subparser.add_parser(
+        name="calc", help="Search Calc developer guide")
+    cmd_draw = subparser.add_parser(
+        name="draw", help="Search Draw developer guide")
+    cmd_chart = subparser.add_parser(
+        name="chart", help="Search Chart developer guide")
+    cmd_base = subparser.add_parser(
+        name="base", help="Search Base developer guide")
+    cmd_form = subparser.add_parser(
+        name="form", help="Search Form developer guide")
+    cmd_general = subparser.add_parser(
+        name="general", help="Search all of developer guide")
     _args_cmd_writer(cmd_writer)
     _args_cmd_calc(cmd_calc)
     _args_cmd_draw(cmd_draw)
@@ -71,4 +96,3 @@ def main() -> int:
     _args_process_cmd(args)
     # print(args)
     return 0
-
